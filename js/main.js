e8ac3166f778e97b2bc88947ac0874fb1cb0da2d
Osama El Henaweey <87882318+osama-elhenaweey@users.noreply.github.com>
2023-11-10 01:08:35 +0200

    Adding testimonials✅

--- js/main.js
+++ js/main.js
@@ -40,3 +40,55 @@ closeSearchBox.addEventListener("click", function () {
     searchBox.classList.remove("active");
 });
 /* ///////////////////// search box END///////////////////// */
+// Slider
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+// slider buttons
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+let maxSlide = slides.length;
+slider.addEventListener("click", function (e) {
+    const clicks = e.target.classList.contains("slider__btn");
+
+    if (!clicks) return;
+    if (clicks) {
+    }
+});
+slides.forEach((el, i) => {
+    el.style.transform = `translateX(${100 * i}%)`;
+});
+const goToSlide = function (slide) {
+    slides.forEach((el, i) => {
+        el.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+};
+goToSlide(0);
+
+const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+    goToSlide(curSlide);
+    activeDot(curSlide);
+};
+const prevSlide = function () {
+    if (curSlide === 0) {
+        curSlide = maxSlide - 1;
+    } else {
+        curSlide--;
+    }
+    goToSlide(curSlide);
+    activeDot(curSlide);
+};
+btnRight.addEventListener("click", nextSlide);
+
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowLeft") prevSlide();
+    if (e.key == "ArrowRight") nextSlide();
+});
